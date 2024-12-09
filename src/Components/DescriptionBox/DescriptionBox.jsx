62ad6c93f929d160b3b66783987201db78bd6908
sathish <sathishbca2003@gmail.com>
2024-12-09 19:16:46 +0530

    initial commit

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Description.css'
+ const DescriptionBox = () => {
+  return (
+    <div className="descriptionbox">
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Decscription</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur at eaque porro iure modi quidem, commodi corporis dolorum distinctio mollitia inventore, voluptatum officia officiis explicabo. Ducimus ab quisquam quis totam?</p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum itaque natus adipisci, animi repellendus alias sed fuga minima, reprehenderit dolore molestiae possimus ab sunt nemo veniam facilis accusantium dolorum earum?
+        </p>
+        </div>
+    </div>
+  )
+}
+export default DescriptionBox
---
+++ src/Pages/Cart.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import CartItems from '../Components/Cartitems/Cartitems';
+
+export const Cart = () => {
+  return (
+    <div>
+      <CartItems />
+    </div>
+  )
+}
+export default Cart;
